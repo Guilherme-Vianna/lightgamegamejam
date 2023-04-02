cb1e08dd0a6a6afbe351d2d05a39689a5579de7d
VandinDev <juniorevandro.gamedev@gmail.com>
2023-04-02 14:45:20 -0300

    feat: Iluminaçao e animação adicionada

--- Assets/Scripts/Entities/Player/PlayerMovement.cs
+++ Assets/Scripts/Entities/Player/PlayerMovement.cs
@@ -5,15 +5,31 @@ namespace Entities.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+<<<<<<< Updated upstream
         public Vector2 movement;
         public int speed = 5;
+=======
+        private Vector2 movement;
+        private int speed = 5;
+        [SerializeField]
+        private Animator Animator;
+>>>>>>> Stashed changes
         
         void Update()
         {
             Move();
         }
 
+<<<<<<< Updated upstream
         private void Move()
+=======
+        private void Update()
+        {
+            Animacoes();
+        }
+
+        void FixedUpdate()
+>>>>>>> Stashed changes
         {
             transform.position += new Vector3(movement.x, movement.y, 0) * Time.deltaTime * speed;
         }
@@ -21,6 +37,20 @@ namespace Entities.Player
         public void SetMovement(InputAction.CallbackContext value)
         {
             movement = value.ReadValue<Vector2>();
+        }
+
+
+        public void Animacoes()
+        {
+            Animator.SetFloat("X", movement.x);
+            Animator.SetFloat("Y", movement.y);
+            Animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            if (movement != Vector2.zero)
+            {
+                Animator.SetFloat("HorizontalIdle", movement.x);
+                Animator.SetFloat("VerticalIdle", movement.y);
+            }
         }
     }
 }
